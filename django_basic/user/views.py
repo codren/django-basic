bef92c653551d4184c4f9f370b1edf7c078b3ecf
codren <tkdrms0291@naver.com>
2021-05-06 10:18:01 +0900

    Login (cookies and session)

--- django_basic/user/views.py
+++ django_basic/user/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 
 def register(request):
     if request.method == 'GET':
@@ -29,3 +29,34 @@ def register(request):
             user.save()
 
     return render(request, 'register.html', res_data)
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+    res_data = {}
+
+    if not (username and password):
+        res_data['error'] = "모든 값을 입력하세요."
+    else:
+        user = User.objects.get(username=username)
+        if check_password(password, user.password):
+            request.session['user'] = user.id
+            return redirect('/')
+        else:
+            res_data['error'] = "비밀번호가 틀렸습니다."
+
+    return render(request, 'login.html', res_data)
+
+        
+def home(request):
+    user_id = request.session.get('user')
+    if user_id:
+        user = User.objects.get(pk=user_id)
+        return HttpResponse(user.username)
+    
+    return HttpResponse('home!')
