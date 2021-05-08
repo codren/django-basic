245de106839c8954827c86909cc8142259233725
codren <tkdrms0291@naver.com>
2021-05-08 22:29:44 +0900

    form class

---
+++ django_basic/user/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import User
+from django.contrib.auth.hashers import check_password
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=32, label="아이디",
+        error_messages = {
+            'required': '아이디를 입력해주세요.'
+        })
+    password = forms.CharField(widget=forms.PasswordInput, label="비밀번호",
+        error_messages = {
+            'required': '비밀번호를 입력해주세요.'
+        })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                self.add_error('username', '해당 아이디가 존재하지 않습니다.')
+                return
+                
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.user_id = user.id
+            
--- django_basic/user/views.py
+++ django_basic/user/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
+from .forms import LoginForm
 
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
-
     elif request.method == 'POST':
-      
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         email = request.POST.get('email', None)
@@ -27,32 +26,19 @@ def register(request):
                 password=make_password(password)
             )
             user.save()
-
     return render(request, 'register.html', res_data)
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-    res_data = {}
-
-    if not (username and password):
-        res_data['error'] = "모든 값을 입력하세요."
-    else:
-        user = User.objects.get(username=username)
-        if check_password(password, user.password):
-            request.session['user'] = user.id
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
             return redirect('/')
-        else:
-            res_data['error'] = "비밀번호가 틀렸습니다."
-
-    return render(request, 'login.html', res_data)
-
-
+    else:
+        form =LoginForm()
+    return render(request, 'login.html', {'form': form})
+    
 
 def logout(request):
     if request.session.get('user'):
@@ -64,5 +50,4 @@ def home(request):
     if user_id:
         user = User.objects.get(pk=user_id)
         return HttpResponse(user.username)
-    
     return HttpResponse('home!')
